10024c0ad1684f7cb6a0db8dc165f10ab39abdd1
Michael Silva <michaelfs@ciandt.com>
2020-11-09 17:23:14 -0300

    Added project files generated by initializr

---
+++ src/main/java/com/marvel/api/MarvelApiApplication.java
@@ -0,0 +1,13 @@
+package com.marvel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelApiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelApiV2Application.class, args);
+	}
+
+}
