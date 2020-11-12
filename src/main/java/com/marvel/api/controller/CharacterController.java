c535b41f98b0fb681d6225edb56f7a4fa88fbf59
Michael Silva <michaelfs@ciandt.com>
2020-11-12 19:04:38 -0300

    Added spock configuration and updated build file

--- src/main/java/com/marvel/api/controller/CharacterController.java
+++ src/main/java/com/marvel/api/controller/CharacterController.java
@@ -3,10 +3,12 @@ package com.marvel.api.controller;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/characters")
 public class CharacterController {
@@ -19,6 +21,7 @@ public class CharacterController {
             @ApiResponse(code = 500, message = "Internal Server Error")})
     @GetMapping
     public String test() {
+        log.info("API is working");
         return "API is working";
     }
 }
---
+++ src/test/groovy/com/marvel/api/controller/CharacterControllerSpec.groovy
@@ -0,0 +1,6 @@
+package com.marvel.api.controller
+
+import spock.lang.Specification
+
+class CharacterControllerSpec extends Specification {
+}
