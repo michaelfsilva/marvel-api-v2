8238c5dd76a819ba57309423d131115ab000e20f
Michael Silva <michaelfs@ciandt.com>
2022-01-25 11:10:40 -0300

    Adding postman collection for testing

--- src/main/java/com/marvel/api/controller/CharacterController.java
+++ src/main/java/com/marvel/api/controller/CharacterController.java
@@ -14,15 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
@@ -74,7 +66,7 @@ public class CharacterController {
   @GetMapping("/findByName/{name}")
   public ResponseEntity<Response<List<CharacterResponse>>> getByName(
       @PathVariable final String name) {
-    log.debug("listing character by name");
+    log.debug("listing characters by name");
     final var characters = characterService.listByName(name);
 
     if (characters.isEmpty()) {
